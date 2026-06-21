ec7b44b3498b56e5d3b45f7fdf6baaa055f102c4
Sethy Rung <rungsethyhk@gmail.com>
2026-06-21 22:44:25 +0700

    feat: wire Hilt application class and entry point

---
+++ app/src/main/java/com/sethy/easypay/EasyPayApp.kt
@@ -0,0 +1,7 @@
+package com.sethy.easypay
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class EasyPayApp : Application()
--- app/src/main/java/com/sethy/easypay/MainActivity.kt
+++ app/src/main/java/com/sethy/easypay/MainActivity.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.sethy.easypay.design.EasyPayTheme
 import com.sethy.easypay.navigation.EasyPayNavGraph
-import com.sethy.easypay.ui.theme.EasyPayTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
